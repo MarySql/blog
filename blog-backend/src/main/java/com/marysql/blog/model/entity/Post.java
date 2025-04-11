a0fb4624647680b02c019c86fa6723bb72fc876e
MarySql <mary.mitsuri250@gmail.com>
2025-04-11 20:33:45 -0300

    fix(entity): adicionar anotações ausentes de Lombok e MongoDB na classe User
    Corrigida a ausência de anotações essenciais na classe User.
    Adicionados @Data para geração automática de getters e setters e @Document para mapeamento da entidade como um documento no MongoDB.

--- blog-backend/src/main/java/com/marysql/blog/model/entity/Post.java
+++ blog-backend/src/main/java/com/marysql/blog/model/entity/Post.java
@@ -1,10 +1,14 @@
 package com.marysql.blog.model.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Document(collation = "posts")
+@Data
 public class Post {
     @Id
     private String id;
