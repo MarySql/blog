75afa873839f2d12ec9fbb157e25849a7b7126d8
MarySql <mary.mitsuri250@gmail.com>
2025-04-11 20:21:53 -0300

    feat(entity): adicionar classe User para gerenciamento de usuários
    Adicionada a classe User como entidade para representar os usuários do sistema.
    Inclui atributos básicos como nome, email, e roles para autenticação e autorização.

---
+++ blog-backend/src/main/java/com/marysql/blog/model/entity/User.java
@@ -0,0 +1,18 @@
+package com.marysql.blog.model.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "users")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password; // Hash da senha (usar BCrypt) <-
+    private List<String> roles; // "ROLE_USER", "ROLE_ADMIN"
+}
