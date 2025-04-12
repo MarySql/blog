8bcc6670f0b8416c96fc61f2ef496db8774f7c97
MarySql <mary.mitsuri250@gmail.com>
2025-04-12 19:06:10 -0300

    feat(dto): adicionar validações para os campos de UserDTO com mensagens personalizadas

--- blog-backend/src/main/java/com/marysql/blog/model/dto/UserDTO.java
+++ blog-backend/src/main/java/com/marysql/blog/model/dto/UserDTO.java
@@ -1,11 +1,21 @@
 package com.marysql.blog.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO {
-
+@NotBlank(message = "Nome do usuário obrigatório")
+@Size(min = 3, max = 20, message = "O nome precisa ter entre 3 e 20 caracteres")
     private String username;
+
+@NotBlank(message = "Email obrigatório")
+@Email(message = "Email precisa ser válido")
     private String email;
+
+@NotBlank(message = "Senha obrigatória")
+@Size(min = 6, message = "A senha precisa ter no mínimo 6 caracteres")
     private String password;
 }
