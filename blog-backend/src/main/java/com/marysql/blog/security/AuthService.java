e4408a83cf3535508f73ae07afb820248ed268ee
MarySql <mary.mitsuri250@gmail.com>
2025-04-12 16:29:16 -0300

    feat(auth): implementar serviço de registro de usuários com validação e codificação de senha

---
+++ blog-backend/src/main/java/com/marysql/blog/security/AuthService.java
@@ -0,0 +1,44 @@
+package com.marysql.blog.security;
+
+import com.marysql.blog.model.dto.UserDTO;
+import com.marysql.blog.model.entity.User;
+import com.marysql.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public String register(UserDTO userDTO) {
+        if (userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        userRepository.save(user);
+
+        return "Usuário registrado com sucesso!"
+    }
+
+
+}
