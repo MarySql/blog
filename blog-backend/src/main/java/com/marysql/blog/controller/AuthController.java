f1f329b70cb8cde9809ad2d5242b0e22c07a8ee2
MarySql <mary.mitsuri250@gmail.com>
2025-04-12 16:41:36 -0300

    feat(controller): adicionar endpoint de registro de usuários na AuthController

---
+++ blog-backend/src/main/java/com/marysql/blog/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.marysql.blog.controller;
+
+import com.marysql.blog.model.dto.UserDTO;
+import com.marysql.blog.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    private ResponseEntity<String> rigister(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(authService.register(userDTO));
+    }
+}
